d0d39a7c794c27c8acc5027b119605650f070224
Aneesh Sharma <aneeshsharma@AneeshMacBook-Pro.local>
2020-06-02 04:33:55 +0530

    make Store persist

--- src/index.js
+++ src/index.js
@@ -7,7 +7,33 @@ import './index.css';
 import reducer from './store/reducer';
 import {BrowserRouter} from 'react-router-dom';
 
-const store = createStore(reducer);
+function saveToLocalStorage(state) {
+  try {
+      let serializedState = JSON.stringify(state);
+      localStorage.setItem("state", serializedState);
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+      let serializedState = localStorage.getItem("state");
+      if (serializedState === null) {
+          return undefined;
+      }
+      return JSON.parse(serializedState);
+  }
+  catch (err) {
+    console.log(err);
+      return undefined;
+  }
+}
+
+const persistedState = loadFromLocalStorage();
+const store = createStore(reducer, persistedState);
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 ReactDOM.render(
   <Provider store={store}>
